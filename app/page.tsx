1519e2c5fd2da76256693d9ecef0d8a62b8d1a55
Sakith B. <pvsakith@gmail.com>
2025-02-27 19:58:01 +0530

    changed metadata

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,18 @@ import { RegistrationSection } from "@/components/layout/sections/registration";
 
 export const metadata = {
   title: "Techie Sleuths",
-  description: "Techie Sleuths",
+  description: "Techie Sleuths event on 5th of March - FOSS NSBM",
   openGraph: {
     type: "website",
-    title: "Techie Sleuths website",
-    description: "Techie Sleuths",
+    url: "https://github.com/fossnsbm/techie-sleuths-25.git",
+    title: "Techie Sleuths",
+    description: "Techie Sleuths event on 5th of March - FOSS NSBM",
     images: [
       {
-        url: "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
-        width: 1200,
-        height: 630,
-        alt: "landing page",
+        url: "https://ts25.fossnsbm.org/sss.png",
+        width: 4200,
+        height: 2500,
+        alt: "Techie Sleuths - FOSS NSBM",
       },
     ],
   } 
